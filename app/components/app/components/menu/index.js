365e1fccecd70bd97920d87d649554e0690f1099
Benoit Koenig <koenigbenoit47@gmail.com>
2019-10-23 19:24:40 +0800

    Add the jumps count in the menu. Timer is ready to be implemented

--- app/components/app/components/menu/index.js
+++ app/components/app/components/menu/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import { StyleSheet, Text, View, Picker } from 'react-native';
-import { CheckBox } from 'react-native-elements'
+import { StyleSheet, Text, View, Picker, TextInput } from 'react-native';
+import { CheckBox, Divider } from 'react-native-elements'
 
 const styles = StyleSheet.create({
   start: {
@@ -26,6 +26,11 @@ const styles = StyleSheet.create({
     marginTop: 16,
   },
 
+  divider: {
+    backgroundColor: '#fafafa',
+    marginHorizontal: 20,
+  },
+
   picker: {
     backgroundColor: '#fafafa',
     height: 20,
@@ -48,21 +53,20 @@ class Menu extends React.Component {
   state = {
     minimumSpeedChecked: false,
     minimumSpeed: 2,
+    timerChecked: false,
+    jumpsCountChecked: false,
+    jumpsCount: '1000',
   }
 
-  toggle = (key) => {
-    return () => this.setState({ [key]: !this.state[key] });
-  }
+  toggleMinimumSpeed = () => this.setState({ minimumSpeedChecked: !this.state.minimumSpeedChecked });
+  toggleTimer = () => this.setState({ timerChecked: !this.state.timerChecked, jumpsCountChecked: false });
+  toggleJumpsCount = () => this.setState({ jumpsCountChecked: !this.state.jumpsCountChecked, timerChecked: false });
 
-  update = (key, keyToValidate) => {
-    return (newValue) => this.setState({
-      [key]: newValue,
-      [keyToValidate]: true,
-    });
-  }
+  updateMinimumSpeed = (newValue) => this.setState({ minimumSpeedChecked: true, minimumSpeed: newValue });
+  updateJumpsCount = (newValue) => this.setState({ jumpsCountChecked: true, timerChecked: false, jumpsCount: newValue });
 
   render() {
-    const { minimumSpeedChecked, minimumSpeed } = this.state;
+    const { minimumSpeedChecked, minimumSpeed, timerChecked, jumpsCountChecked, jumpsCount } = this.state;
 
     return (
       <View>
@@ -71,14 +75,24 @@ class Menu extends React.Component {
         </View>
         <View>
           <View style={styles.option}>
-            <CheckBox checkedColor='#00d0c0' checked={minimumSpeedChecked} onPress={this.toggle('minimumSpeedChecked')} />
+            <CheckBox checkedColor='#00d0c0' checked={minimumSpeedChecked} onPress={this.toggleMinimumSpeed} />
             <Text style={styles.optionText}>Set minimum speed</Text>
-            <Picker style={minimumSpeedChecked ? styles.picker: styles.pickerUnselected} selectedValue={minimumSpeed} onValueChange={this.update('minimumSpeed', 'minimumSpeedChecked')}>
+            <Picker style={minimumSpeedChecked ? styles.picker: styles.pickerUnselected} selectedValue={minimumSpeed} onValueChange={this.updateMinimumSpeed}>
               {
                 Array(20).fill(0).map((_, i) => (<Picker.Item key={i} label={(2 + i / 10) + ''} value={(2 + i / 10)} />))
               }
             </Picker>
             <Text style={styles.optionText}>jumps / sec</Text>
+          </View>
+          <Divider style={styles.divider} />
+          <View style={styles.option}>
+            <CheckBox checkedColor='#00d0c0' checked={timerChecked} onPress={this.toggleTimer} />
+            <Text style={styles.optionText}>Set a timer</Text>
+          </View>
+          <View style={styles.option}>
+            <CheckBox checkedColor='#00d0c0' checked={jumpsCountChecked} onPress={this.toggleJumpsCount} />
+            <Text style={styles.optionText}>Set a jump count</Text>
+            <TextInput style={jumpsCountChecked ? styles.picker: styles.pickerUnselected} keyboardType='number-pad' onChangeText={this.updateJumpsCount} value={jumpsCount} />
           </View>
         </View>
       </View>
